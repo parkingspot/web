78c468917c65bf4900e16716c2efaab0f96e8309
rafalopezgit <rafael.lopezgm@gmail.com>
2018-04-05 20:51:15 +0200

    signup petition works

--- src/app/components/misc/signup/signup.component.ts
+++ src/app/components/misc/signup/signup.component.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from '../../../shared/models/user.model';
+import { UsersService } from './../../../shared/services/users.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent {
   user: User = new User();
-  constructor() { }
+  apiError: string
 
-  onSubmit() {
-    console.log(this.user);
+  constructor(
+    private router: Router,
+    private usersService: UsersService
+  ) {}
+
+
+  onSubmitSignup(signupForm) {
+    this.usersService.create(this.user).subscribe(
+      (user) => {
+        this.router.navigate(['/login']);
+      },
+      (error) => {
+        console.log(error)
+        this.apiError = error.message;
+      }
+    );
   }
-
-  ngOnInit() {
-  }
-
+  
 }
