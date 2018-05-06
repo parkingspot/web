7e6399aa4fe34624ff5297896f770a341e4e9865
Miguel Manzano <miguelmanzan@gmail.com>
2018-05-06 18:32:59 +0200

    Close Collapse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {NavComponent} from './components/misc/nav/nav.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  @ViewChild(NavComponent) child: any;
+
+  closeButton() {
+    this.child.onclickClose();
+  }
 }
